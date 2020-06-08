c4cb8c13333b4d7b99da703f4aeefc942ccc6ae1
samirasalah <47776331+samirasalah@users.noreply.github.com>
2020-06-08 11:54:17 +0100

    addFirebaseRegistration

--- lib/screens/authenticate/authenticate.dart
+++ lib/screens/authenticate/authenticate.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutterapp12/screens/home/hello.dart';
 
@@ -40,11 +42,17 @@ class MyHome extends StatefulWidget {
 }
 class _MyHomePageState extends State<MyHome> {
   TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+  TextEditingController cinController=TextEditingController();
+    TextEditingController nameController=TextEditingController();
+      TextEditingController emailController=TextEditingController();
+        TextEditingController passwordController=TextEditingController();
+          TextEditingController numeroController=TextEditingController();
 
   @override
   Widget build(BuildContext context) {
 
     final cinField = TextField(
+      controller:  cinController,
       obscureText: false,
       style: style,
       decoration: InputDecoration(
@@ -54,6 +62,7 @@ class _MyHomePageState extends State<MyHome> {
           OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
     );
     final nameField = TextField(
+      controller: nameController,
       obscureText: false,
       style: style,
       decoration: InputDecoration(
@@ -63,6 +72,7 @@ class _MyHomePageState extends State<MyHome> {
           OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
     );
     final emailField = TextField(
+      controller: emailController,
       obscureText: false,
       style: style,
       decoration: InputDecoration(
@@ -72,6 +82,7 @@ class _MyHomePageState extends State<MyHome> {
           OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
     );
     final numField = TextField(
+      controller: numeroController,
       obscureText: false,
       style: style,
       decoration: InputDecoration(
@@ -81,6 +92,7 @@ class _MyHomePageState extends State<MyHome> {
           OutlineInputBorder(borderRadius: BorderRadius.circular(32.0))),
     );
     final passwordField = TextField(
+      controller: passwordController,
       obscureText: true,
       style: style,
       decoration: InputDecoration(
@@ -98,56 +110,116 @@ class _MyHomePageState extends State<MyHome> {
         minWidth: MediaQuery.of(context).size.width,
         padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
         onPressed: () {
-          Navigator.of(context).pushNamed("/sidebar");
-        },
-        child: Text("Inscription",
-            textAlign: TextAlign.center,
-            style: style.copyWith(
-                color: Colors.white, fontWeight: FontWeight.bold)),
-      ),
-    );
-    return Scaffold(
-        appBar:AppBar(
-        backgroundColor: Colors.purple,
-        title: Text('Baladity'),
-        ),
-      body: Center(
-        child: Container(
-          color: Colors.white,
-          child: Padding(
-            padding: const EdgeInsets.all(36.0),
-            child: ListView(
-
-              children: <Widget>[
-             SizedBox(
-                  height: 155.0,
-                  child: Image.asset(
-                    "a7/logo.png",
-                    fit: BoxFit.contain,
+          registerFirebase();
+                   // Navigator.of(context).pushNamed("/sidebar");
+                  },
+                  child: Text("Inscription",
+                      textAlign: TextAlign.center,
+                      style: style.copyWith(
+                          color: Colors.white, fontWeight: FontWeight.bold)),
+                ),
+              );
+              return Scaffold(
+                  appBar:AppBar(
+                  backgroundColor: Colors.purple,
+                  title: Text('Baladity'),
+                  ),
+                body: Center(
+                  child: Container(
+                    color: Colors.white,
+                    child: Padding(
+                      padding: const EdgeInsets.all(36.0),
+                      child: ListView(
+          
+                        children: <Widget>[
+                       SizedBox(
+                            height: 155.0,
+                            child: Image.asset(
+                              "a7/logo.png",
+                              fit: BoxFit.contain,
+                            ),
+                          ),
+                          SizedBox(height: 25.0),
+                          cinField,
+                          SizedBox(height:25.0),
+                          nameField,
+                          SizedBox(height: 25.0),
+                          emailField,
+                          SizedBox(height: 25.0),
+                          passwordField,
+                          SizedBox(height: 25.0),
+                          numField,
+                          SizedBox(
+                            height: 15.0,
+                          ),
+                          creatButon,
+                          SizedBox(
+                            height: 15.0,
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
                 ),
-                SizedBox(height: 25.0),
-                cinField,
-                SizedBox(height:25.0),
-                nameField,
-                SizedBox(height: 25.0),
-                emailField,
-                SizedBox(height: 25.0),
-                passwordField,
-                SizedBox(height: 25.0),
-                numField,
-                SizedBox(
-                  height: 15.0,
-                ),
-                creatButon,
-                SizedBox(
-                  height: 15.0,
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
+              );
+            }
+          
+            Future<void> registerFirebase() async {
+              String errorMsg;
+                try {
+               AuthResult authResult = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: emailController.text,password: passwordController.text) ;
+                FirebaseUser user=authResult.user;
+                  UserUpdateInfo userUpdateInfo = new UserUpdateInfo();
+                  userUpdateInfo.displayName = nameController.text;
+                  user.updateProfile(userUpdateInfo).then((onValue) {
+                Navigator.of(context).pushNamed("/sidebar");
+                Firestore.instance.collection('users').document().setData(
+                    {'email': emailController.text, 'displayName': nameController.text,'displayCIN': cinController.text,'displayNumero': numeroController.text}).then((onValue) {
+                 
+                });
+              });
+                } catch (error) {
+          switch (error.code) {
+            case "ERROR_EMAIL_ALREADY_IN_USE":
+              {
+                
+                  errorMsg = "This email is already in use.";
+               
+              
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        content: Container(
+                          child: Text(errorMsg),
+                        ),
+                      );
+                    });
+              }
+              break;
+            case "ERROR_WEAK_PASSWORD":
+              {
+              
+                  errorMsg = "The password must be 6 characters long or more.";
+                
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        content: Container(
+                          child: Text(errorMsg),
+                        ),
+                      );
+                    });
+              }
+              break;
+            default:
+              {
+              
+                  errorMsg = "";
+               
+              }
+          }
+            
+                }}
 }
